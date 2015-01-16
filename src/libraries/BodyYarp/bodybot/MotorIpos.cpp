eba8177ce654e085a612a93c11b98839c88f2179
jgvictores <jgvictores@3ee78acd-4d09-4874-990d-b3b85ff74acd>
2015-01-16 17:44:47 +0000

    =

--- src/libraries/BodyYarp/bodybot/MotorIpos.cpp
+++ src/libraries/BodyYarp/bodybot/MotorIpos.cpp
@@ -386,13 +386,11 @@ bool MotorIpos::interpretMessage( can_msg * message) {
 
 std::string MotorIpos::msgToStr(can_msg* message) {
     std::stringstream tmp;
-    /*for(int i=0; i < message->dlc; i++)
+    for(int i=0; i < message->dlc; i++)
     {
-        tmp << std::dec << message->data[i];
-    }*/
-    tmp << std::hex << static_cast<int>(message->data[0]);
-    tmp << "-";
-    tmp << "canId(";
+        tmp << std::hex << static_cast<int>(message->data[i]);
+    }
+    tmp << " canId(";
     tmp << (message->id & 0x7F);
     tmp << ") via(";
     tmp << std::hex << (message->id & 0xFF80);
