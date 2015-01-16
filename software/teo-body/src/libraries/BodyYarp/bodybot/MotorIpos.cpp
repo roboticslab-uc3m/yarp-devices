16e4f1a1ab18b702e740e9c8404e876354e32408
jgvictores <jgvictores@3ee78acd-4d09-4874-990d-b3b85ff74acd>
2015-01-16 10:17:16 +0000

    =

--- software/teo-body/src/libraries/BodyYarp/bodybot/MotorIpos.cpp
+++ software/teo-body/src/libraries/BodyYarp/bodybot/MotorIpos.cpp
@@ -260,63 +260,63 @@ bool MotorIpos::interpretMessage( can_msg * message) {
 
     if( (message->id-canId) == 0x80 )  // EMERGENCY (EMCY), Table 4.2 Emergency Error Codes (p57, 73/263)
     {
+        CD_ERROR("Got EMERGENCY from iPOS: %X %X %X %X %X %X %X %X. canId: %d (via %X): ",
+            message->data[0],message->data[1],
+            message->data[2],message->data[3],
+            message->data[4],message->data[5],
+            message->data[6],message->data[7],
+            canId,message->id-canId);
         if( (message->data[1]==0x00)&&(message->data[0]==0x00) ) {
-            CD_ERROR("Error Reset or No Error.\n");
+            CD_ERROR_NO_HEADER("Error Reset or No Error.\n");
         } else if ( (message->data[1]==0x10)&&(message->data[0]==0x00) ) {
-            CD_ERROR("Generic error.\n");
+            CD_ERROR_NO_HEADER("Generic error.\n");
         } else if ( (message->data[1]==0x23)&&(message->data[0]==0x10) ) {
-            CD_ERROR("Continuous over-current.\n");
+            CD_ERROR_NO_HEADER("Continuous over-current.\n");
         } else if ( (message->data[1]==0x23)&&(message->data[0]==0x40) ) {
-            CD_ERROR("Short-circuit.\n");
+            CD_ERROR_NO_HEADER("Short-circuit.\n");
         } else if ( (message->data[1]==0x32)&&(message->data[0]==0x10) ) {
-            CD_ERROR("DC-link over-voltage.\n");
+            CD_ERROR_NO_HEADER("DC-link over-voltage.\n");
         } else if ( (message->data[1]==0x32)&&(message->data[0]==0x20) ) {
-            CD_ERROR("DC-link under-voltage.\n");
+            CD_ERROR_NO_HEADER("DC-link under-voltage.\n");
         } else if ( (message->data[1]==0x42)&&(message->data[0]==0x80) ) {
-            CD_ERROR("Over temperature motor.\n");
+            CD_ERROR_NO_HEADER("Over temperature motor.\n");
         } else if ( (message->data[1]==0x43)&&(message->data[0]==0x10) ) {
-            CD_ERROR("Over temperature drive.\n");
+            CD_ERROR_NO_HEADER("Over temperature drive.\n");
         } else if ( (message->data[1]==0x54)&&(message->data[0]==0x41) ) {
-            CD_ERROR("Driver disabled due to enable input.\n");
+            CD_ERROR_NO_HEADER("Driver disabled due to enable input.\n");
         } else if ( (message->data[1]==0x54)&&(message->data[0]==0x42) ) {
-            CD_ERROR("Negative limit switch active.\n");
+            CD_ERROR_NO_HEADER("Negative limit switch active.\n");
         } else if ( (message->data[1]==0x54)&&(message->data[0]==0x43) ) {
-            CD_ERROR("Positive limit switch active.\n");
+            CD_ERROR_NO_HEADER("Positive limit switch active.\n");
         } else if ( (message->data[1]==0x61)&&(message->data[0]==0x00) ) {
-            CD_ERROR("Invalid setup data.\n");
+            CD_ERROR_NO_HEADER("Invalid setup data.\n");
         } else if ( (message->data[1]==0x75)&&(message->data[0]==0x00) ) {
-            CD_ERROR("Communication error.\n");
+            CD_ERROR_NO_HEADER("Communication error.\n");
         } else if ( (message->data[1]==0x81)&&(message->data[0]==0x10) ) {
-            CD_ERROR("CAN overrun (message lost).\n");
+            CD_ERROR_NO_HEADER("CAN overrun (message lost).\n");
         } else if ( (message->data[1]==0x81)&&(message->data[0]==0x30) ) {
-            CD_ERROR("Life guard error or heartbeat error.\n");
+            CD_ERROR_NO_HEADER("Life guard error or heartbeat error.\n");
         } else if ( (message->data[1]==0x83)&&(message->data[0]==0x31) ) {
-            CD_ERROR("I2t protection triggered.\n");
+            CD_ERROR_NO_HEADER("I2t protection triggered.\n");
         } else if ( (message->data[1]==0x85)&&(message->data[0]==0x80) ) {
-            CD_ERROR("Position wraparound / Hal sensor missing.\n");
+            CD_ERROR_NO_HEADER("Position wraparound / Hal sensor missing.\n");
         } else if ( (message->data[1]==0x86)&&(message->data[0]==0x11) ) {
-            CD_ERROR("Control error / Following error.\n");
+            CD_ERROR_NO_HEADER("Control error / Following error.\n");
         } else if ( (message->data[1]==0x90)&&(message->data[0]==0x00) ) {
-            CD_ERROR("Command error\n");
+            CD_ERROR_NO_HEADER("Command error\n");
         } else if ( (message->data[1]==0xFF)&&(message->data[0]==0x01) ) {
-            CD_ERROR("Generic interpolated position mode error ( PVT / PT error.\n");
+            CD_ERROR_NO_HEADER("Generic interpolated position mode error ( PVT / PT error.\n");
         } else if ( (message->data[1]==0xFF)&&(message->data[0]==0x02) ) {
-            CD_ERROR("Change set acknowledge bit wrong value.\n");
+            CD_ERROR_NO_HEADER("Change set acknowledge bit wrong value.\n");
         } else if ( (message->data[1]==0xFF)&&(message->data[0]==0x03) ) {
-            CD_ERROR("Specified homing method not available.\n");
+            CD_ERROR_NO_HEADER("Specified homing method not available.\n");
         } else if ( (message->data[1]==0xFF)&&(message->data[0]==0x04) ) {
-            CD_ERROR("A wrong mode is set in object 6060h, modes_of_operation.\n");
+            CD_ERROR_NO_HEADER("A wrong mode is set in object 6060h, modes_of_operation.\n");
         } else if ( (message->data[1]==0xFF)&&(message->data[0]==0x05) ) {
-            CD_ERROR("Specified digital I/O line not available.\n");
+            CD_ERROR_NO_HEADER("Specified digital I/O line not available.\n");
         } else {
-            CD_ERROR("Got EMERGENCY from driver side: unknown, %X %X %X %X %X %X %X %X. canId: %d (via %X).\n",
-                message->data[0],message->data[1],
-                message->data[2],message->data[3],
-                message->data[4],message->data[5],
-                message->data[6],message->data[7],
-                canId,message->id-canId);
+            CD_ERROR_NO_HEADER("NOT SPECIFIED IN MANUAL.\n");
         }
-
 
         return true;
     }
