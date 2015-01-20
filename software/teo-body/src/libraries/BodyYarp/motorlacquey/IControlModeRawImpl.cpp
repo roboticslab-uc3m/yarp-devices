2042e5318a2363ad7c702c5346cc8ab2d40c89fe
jgvictores <jgvictores@3ee78acd-4d09-4874-990d-b3b85ff74acd>
2015-01-20 15:05:29 +0000

    =

--- software/teo-body/src/libraries/BodyYarp/motorlacquey/IControlModeRawImpl.cpp
+++ software/teo-body/src/libraries/BodyYarp/motorlacquey/IControlModeRawImpl.cpp
@@ -10,17 +10,6 @@ bool teo::MotorLacquey::setPositionModeRaw(int j) {
     //-- Check index within range
     if ( j != 0 ) return false;
 
-    //*************************************************************
-    uint8_t msg_position_mode[] = {0x2F,0x60,0x60,0x00,0x01};  // Position mode
-
-    if( ! send( 0x600, 5, msg_position_mode) )
-    {
-        CD_ERROR("Could not send \"position_mode\". %s\n", msgToStr(0x600, 5, msg_position_mode).c_str() );
-        return false;
-    }
-    CD_SUCCESS("Sent \"position_mode\". %s\n", msgToStr(0x600, 5, msg_position_mode).c_str() );
-    //*************************************************************
-
     return true;
 }
 
@@ -32,17 +21,6 @@ bool teo::MotorLacquey::setVelocityModeRaw(int j) {
     //-- Check index within range
     if ( j != 0 ) return false;
 
-    //*************************************************************
-    uint8_t msg_velocity_mode[]={0x2F,0x60,0x60,0x00,0x03}; // Velocity mode
-
-    if( ! send( 0x600, 5, msg_velocity_mode) )
-    {
-        CD_ERROR("Could not send \"vel_mode\". %s\n", msgToStr(0x600, 5, msg_velocity_mode).c_str() );
-        return false;
-    }
-    CD_SUCCESS("Sent \"vel_mode\". %s\n", msgToStr(0x600, 5, msg_velocity_mode).c_str() );
-    //*************************************************************
-
     return true;
 }
 
@@ -53,38 +31,6 @@ bool teo::MotorLacquey::setTorqueModeRaw(int j)  {
 
     //-- Check index within range
     if ( j != 0 ) return false;
-
-    //*************************************************************
-    //-- External reference type. Slave receives reference through CAN (manual 208 of 263).
-    uint8_t msg_ref_type[]={0x2B,0x1D,0x20,0x00,0x01,0x00,0x00,0x00};  //CAN
-
-    if( ! send( 0x600, 8, msg_ref_type) )
-    {
-        CD_ERROR("Could not send \"ref_type\". %s\n", msgToStr(0x600, 8, msg_ref_type).c_str() );
-        return false;
-    }
-    CD_SUCCESS("Sent \"ref_type\". %s\n", msgToStr(0x600, 8, msg_ref_type).c_str() );
-    //*************************************************************
-    //-- Mode -5 (manual 209 of 263).
-    uint8_t msg_mode_torque[]={0x2F,0x60,0x60,0x00,0xFB,0x00,0x00,0x00};
-
-    if( ! send( 0x600, 8, msg_mode_torque) )
-    {
-        CD_ERROR("Could not send \"mode_torque\". %s\n", msgToStr(0x600, 8, msg_mode_torque).c_str() );
-        return false;
-    }
-    CD_SUCCESS("Sent \"mode_torque\". %s\n", msgToStr(0x600, 8, msg_mode_torque).c_str() );
-    //*************************************************************
-    //-- Control word (manual 215 of 263).
-    uint8_t msg_torque_word[] = {0x1F,0x00};
-
-    if( ! send( 0x200, 2, msg_torque_word) )
-    {
-        CD_ERROR("Could not send msg_torque_word. %s\n", msgToStr(0x200, 2, msg_torque_word).c_str() );
-        return false;
-    }
-    CD_SUCCESS("Sent \"torque_word\". %s\n", msgToStr(0x200, 2, msg_torque_word).c_str() );
-    //*************************************************************
 
     return true;
 }
