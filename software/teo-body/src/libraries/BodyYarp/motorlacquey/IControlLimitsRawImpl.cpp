f4a3eb71dcda974c385677227c89aa2858a5dabc
jgvictores <jgvictores@3ee78acd-4d09-4874-990d-b3b85ff74acd>
2015-01-20 10:54:53 +0000

    =

--- software/teo-body/src/libraries/BodyYarp/motorlacquey/IControlLimitsRawImpl.cpp
+++ software/teo-body/src/libraries/BodyYarp/motorlacquey/IControlLimitsRawImpl.cpp
@@ -10,32 +10,6 @@ bool teo::MotorLacquey::setLimitsRaw(int axis, double min, double max) {
     //-- Check index within range
     if ( axis != 0 ) return false;
 
-    //*************************************************************
-    uint8_t msg_position_min[]={0x23,0x7D,0x60,0x01,0x00,0x00,0x00,0x00}; // 0x01 is subindex 1
-
-    int sendMin = min * this->tr * 11.11112;  // Appply tr & convert units to encoder increments
-    memcpy(msg_position_min+4,&sendMin,4);
-
-    if( ! send( 0x600, 8, msg_position_min ) )
-    {
-        CD_ERROR("Could not send position min. %s\n", msgToStr(0x600, 8, msg_position_min).c_str() );
-        return false;
-    }
-    CD_SUCCESS("Sent \"position min\". %s\n", msgToStr(0x600, 8, msg_position_min).c_str() );
-    //*************************************************************
-    uint8_t msg_position_max[]={0x23,0x7D,0x60,0x02,0x00,0x00,0x00,0x00}; // 0x02 is subindex 2
-
-    int sendMax = max * this->tr * 11.11112;  // Appply tr & convert units to encoder increments
-    memcpy(msg_position_max+4,&sendMax,4);
-
-    if( ! send( 0x600, 8, msg_position_max ) )
-    {
-        CD_ERROR("Could not send position max. %s\n", msgToStr(0x600, 8, msg_position_max).c_str() );
-        return false;
-    }
-    CD_SUCCESS("Sent \"position max\".. %s\n", msgToStr(0x600, 8, msg_position_max).c_str() );
-    //*************************************************************
-
     //-- Store the new limits locally.
     this->max;
     this->min;
