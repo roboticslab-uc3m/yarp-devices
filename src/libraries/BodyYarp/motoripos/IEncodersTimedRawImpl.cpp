4c88ea55d40334bb8597cffe43ccbc7bb763214f
jgvictores <jgvictores@3ee78acd-4d09-4874-990d-b3b85ff74acd>
2015-01-20 10:32:37 +0000

    =

--- src/libraries/BodyYarp/motoripos/IEncodersTimedRawImpl.cpp
+++ src/libraries/BodyYarp/motoripos/IEncodersTimedRawImpl.cpp
@@ -7,7 +7,26 @@
 bool teo::MotorIpos::getEncoderTimedRaw(int j, double *encs, double *time) {
     CD_INFO("(%d)\n",j);
 
-    CD_WARNING("Not implemented yet.\n");
+    //-- Check index within range
+    if ( j != 0 ) return false;
+
+    //*************************************************************
+    uint8_t msg_read[]={0x40,0x64,0x60,0x00,0x00,0x00,0x00,0x00}; // Query position.
+    if( ! send( 0x600, 8, msg_read) )
+    {
+        CD_ERROR("Could not send \"read encoder\". %s\n", msgToStr(0x600, 8, msg_read).c_str() );
+        return false;
+    }
+    //* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    Time::delay(DELAY);  // Must delay as it will be from same driver.
+    //* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+    encoderReady.wait();
+    *encs = encoder;
+    *time = encoderTimestamp;
+    encoderReady.post();
+
+    //*************************************************************
 
     return true;
 }
