d0d81e5fbceeabd1680361e59fb4be81fce2d03c
jgvictores <jgvictores@3ee78acd-4d09-4874-990d-b3b85ff74acd>
2015-01-21 11:20:11 +0000

    =

--- software/teo-body/src/libraries/BodyYarp/motoripos/IControlModeRawImpl.cpp
+++ software/teo-body/src/libraries/BodyYarp/motoripos/IControlModeRawImpl.cpp
@@ -156,7 +156,18 @@ bool teo::MotorIpos::getControlModeRaw(int j, int *mode) {
 
     //-- Ya de paso...
     //*************************************************************
-    uint8_t msgStatus[] = {0x40,0x41,0x60,0x00,0x00,0x00,0x00,0x00}; // Manual 6041h: Status word
+    //uint8_t msgStatus[] = {0x40,0x41,0x60,0x00,0x00,0x00,0x00,0x00}; // Manual 6041h: Status word
+    //if( ! send( 0x600, 8, msgStatus))
+    //{
+    //    CD_ERROR("Could not send status query. %s\n", msgToStr(0x600, 8, msgStatus).c_str() );
+    //    return false;
+    //}
+    //CD_SUCCESS("Sent status query. %s\n", msgToStr(0x600, 8, msgStatus).c_str() );
+    //*************************************************************
+
+    //-- Ya de paso...
+    //*************************************************************
+    uint8_t msgStatus[] = {0x40,0x02,0x10,0x00,0x00,0x00,0x00,0x00}; // Manual 6041h: Status word
     if( ! send( 0x600, 8, msgStatus))
     {
         CD_ERROR("Could not send status query. %s\n", msgToStr(0x600, 8, msgStatus).c_str() );
