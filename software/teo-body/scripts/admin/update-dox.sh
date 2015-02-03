945cb4250f4c6f5e2fa2119b72ffc75bfcacf85d
jgvictores <jgvictores@3ee78acd-4d09-4874-990d-b3b85ff74acd>
2015-02-03 08:50:23 +0000

    =

---
+++ software/teo-body/scripts/admin/update-dox.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Thanks: http://stackoverflow.com/questions/14710257/running-a-cron-job-at-230-am-every-day
+# On how to automate process at 2:30 every day (type "date" to get your server time)
+# crontab -e
+# 30 2 * * * /your/command
+
+echo "Update teo-body..."
+cd $HOME/teo-body
+svn up --username wikirobots
+echo "Doxy teo-body..."
+cd doc
+rm -r html
+/usr/local/bin/doxygen
+cd ../..
+
