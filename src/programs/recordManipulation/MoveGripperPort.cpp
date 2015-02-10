864ca4bbf78e78e0a5d44b60fd326692950a9b39
jgvictores <jgvictores>
2015-02-10 13:02:44 +0100

    more like controlboard

--- src/programs/recordManipulation/MoveGripperPort.cpp
+++ src/programs/recordManipulation/MoveGripperPort.cpp
@@ -9,8 +9,24 @@ void MoveGripperPort::onRead(yarp::os::Bottle &in) {
     iPositionControlLeft->getAxes(&armSizeLeft);
     iPositionControlRight->getAxes(&armSizeRight);
 
-    int j = in.get(0).asInt();
-    int ref = in.get(1).asInt();
+    if( in.size() != 4 ) {
+        CD_ERROR("Only '[set] [pos] axis ref' for now\n");
+        return;
+    }
+
+    if( (in.get(0).asVocab() != VOCAB_SET) || (in.get(0).asString() != "set")  ) {
+        CD_ERROR("Only '[set] [pos] axis ref' for now\n");
+        return;
+    }
+
+    if( (in.get(1).asVocab() != VOCAB_POSITION) || (in.get(0).asString() != "pos")  ) {
+        CD_ERROR("Only '[set] [pos] axis ref' for now\n");
+        return;
+    }
+
+    int j = in.get(2).asInt();
+    double ref = in.get(3).asDouble();
+
     if(j==0)
         iPositionControlLeft->positionMove(armSizeLeft-1,ref);
     else if (j==1)
