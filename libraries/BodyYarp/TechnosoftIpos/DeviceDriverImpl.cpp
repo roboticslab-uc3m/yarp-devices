c8d86f44986bcf37098d43ff6f4b15a9d9fa8870
jgvictores <jgvictores@github.com>
2015-04-15 13:29:19 +0200

    TechnosoftIpos force user to put canId and tr

--- libraries/BodyYarp/TechnosoftIpos/DeviceDriverImpl.cpp
+++ libraries/BodyYarp/TechnosoftIpos/DeviceDriverImpl.cpp
@@ -5,10 +5,10 @@
 // -----------------------------------------------------------------------------
 bool teo::TechnosoftIpos::open(Searchable& config) {
 
-    this->canId = config.check("canId",0,"can bus ID").asInt();
-    this->tr = config.check("tr",0,"reduction").asDouble();
-    this->k = config.check("k",0,"motor constant").asDouble();
-    this->ptModeMs  = config.check("ptModeMs",0,"ptMode ms").asInt();
+    this->canId = config.check("canId",-1,"can bus ID").asInt();
+    this->tr = config.check("tr",-1,"reduction").asDouble();
+    this->k = config.check("k",-1,"motor constant").asDouble();
+    this->ptModeMs  = config.check("ptModeMs",-1,"ptMode ms").asInt();
     this->ptPointCounter = 0;
     this->ptMovementDone = false;
     this->targetReached = false;
@@ -17,6 +17,15 @@ bool teo::TechnosoftIpos::open(Searchable& config) {
     this->refAcceleration = 0;
     this->refSpeed = 0;
     this->encoder = 0;
+
+    if( -1 == this->canId ) {
+        CD_ERROR("Could not create TechnosoftIpos with canId -1");
+        return false;
+    }
+    if( -1 == this->tr ) {
+        CD_ERROR("Could not create TechnosoftIpos with tr -1");
+        return false;
+    }
 
     CD_SUCCESS("Created TechnosoftIpos with canId %d, tr %f, k %f, and all local parameters set to 0.\n",canId,tr,k);
     return true;
