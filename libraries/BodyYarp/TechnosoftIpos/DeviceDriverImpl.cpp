0f1ddad8f783de7c857c6d139c3f64186567fecc
jgvictores <jgvictores@github.com>
2015-04-29 11:45:38 +0200

    TechnosoftIpos better check for zero

--- libraries/BodyYarp/TechnosoftIpos/DeviceDriverImpl.cpp
+++ libraries/BodyYarp/TechnosoftIpos/DeviceDriverImpl.cpp
@@ -5,12 +5,12 @@
 // -----------------------------------------------------------------------------
 bool teo::TechnosoftIpos::open(Searchable& config) {
 
-    this->canId = config.check("canId",-1,"can bus ID").asInt();
-    this->tr = config.check("tr",-1,"reduction").asDouble();
-    this->k = config.check("k",-1,"motor constant").asDouble();
-    this->refAcceleration = config.check("refAcceleration",-1,"ref acceleration").asDouble();
-    this->refSpeed = config.check("refSpeed",-1,"ref speed").asDouble();
-    this->ptModeMs  = config.check("ptModeMs",-1,"ptMode ms").asInt();
+    this->canId = config.check("canId",0,"can bus ID").asInt();
+    this->tr = config.check("tr",0,"reduction").asDouble();
+    this->k = config.check("k",0,"motor constant").asDouble();
+    this->refAcceleration = config.check("refAcceleration",0,"ref acceleration").asDouble();
+    this->refSpeed = config.check("refSpeed",0,"ref speed").asDouble();
+    this->ptModeMs  = config.check("ptModeMs",0,"ptMode ms").asInt();
     this->ptPointCounter = 0;
     this->ptMovementDone = false;
     this->targetReached = false;
@@ -20,20 +20,20 @@ bool teo::TechnosoftIpos::open(Searchable& config) {
     this->refSpeed = 0;
     this->encoder = 0;
 
-    if( -1 == this->canId ) {
-        CD_ERROR("Could not create TechnosoftIpos with canId -1\n");
+    if( 0 == this->canId ) {
+        CD_ERROR("Could not create TechnosoftIpos with canId 0\n");
         return false;
     }
-    if( -1 == this->tr ) {
-        CD_ERROR("Could not create TechnosoftIpos with tr -1\n");
+    if( 0 == this->tr ) {
+        CD_ERROR("Could not create TechnosoftIpos with tr 0\n");
         return false;
     }
-    if( -1 == this->refAcceleration ) {
-        CD_ERROR("Could not create TechnosoftIpos with refAcceleration -1\n");
+    if( 0 == this->refAcceleration ) {
+        CD_ERROR("Could not create TechnosoftIpos with refAcceleration 0\n");
         return false;
     }
-    if( -1 == this->refSpeed ) {
-        CD_ERROR("Could not create TechnosoftIpos with refSpeed -1\n");
+    if( 0 == this->refSpeed ) {
+        CD_ERROR("Could not create TechnosoftIpos with refSpeed 0\n");
         return false;
     }
 
