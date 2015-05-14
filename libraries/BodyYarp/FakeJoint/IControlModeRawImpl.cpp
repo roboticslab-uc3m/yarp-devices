64e552be2355406023353767d35667336f0fa3cf
jgvictores <jgvictores@github.com>
2015-05-14 10:12:02 +0200

    add new FakeJoint to BodyYarp

---
+++ libraries/BodyYarp/FakeJoint/IControlModeRawImpl.cpp
@@ -0,0 +1,84 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "FakeJoint.hpp"
+
+// ------------------- IControlModeRaw Related ------------------------------------
+
+bool teo::FakeJoint::setPositionModeRaw(int j) {
+    CD_INFO("(%d)\n",j);
+
+    //-- Check index within range
+    if ( j != 0 ) return false;
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::FakeJoint::setVelocityModeRaw(int j) {
+    CD_INFO("(%d)\n",j);
+
+    //-- Check index within range
+    if ( j != 0 ) return false;
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::FakeJoint::setTorqueModeRaw(int j)  {
+    CD_INFO("(%d)\n",j);
+
+    //-- Check index within range
+    if ( j != 0 ) return false;
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::FakeJoint::setImpedancePositionModeRaw(int j) {
+    CD_INFO("(%d)\n",j);
+
+    //-- Check index within range
+    if ( j != 0 ) return false;
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::FakeJoint::setImpedanceVelocityModeRaw(int j) {
+    CD_INFO("(%d)\n",j);
+
+    //-- Check index within range
+    if ( j != 0 ) return false;
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::FakeJoint::setOpenLoopModeRaw(int j) {
+    CD_INFO("(%d)\n",j);
+
+    //-- Check index within range
+    if ( j != 0 ) return false;
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool teo::FakeJoint::getControlModeRaw(int j, int *mode) {
+    //CD_INFO("(%d)\n",j);  //-- Too verbose in controlboardwrapper2 stream
+
+    //-- Check index within range
+    if ( j != 0 ) return false;
+
+    *mode = VOCAB_POSITION_MODE;
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
