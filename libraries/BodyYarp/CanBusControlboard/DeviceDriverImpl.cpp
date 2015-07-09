c85907088ee01cf159b5a6288de8b2ed7a28faf8
jgvictores <jcgvicto@gmail.com>
2015-07-09 15:21:44 +0200

    Associate absolute encoders to motor drivers

--- libraries/BodyYarp/CanBusControlboard/DeviceDriverImpl.cpp
+++ libraries/BodyYarp/CanBusControlboard/DeviceDriverImpl.cpp
@@ -77,6 +77,12 @@ bool teo::CanBusControlboard::open(Searchable& config) {
         driver->view( iVelocityControlRaw[i] );
         driver->view( iCanBusSharer[i] );
 
+        //-- Associate absolute encoders to motor drivers
+        if( "CuiAbsolute" == types.get(i).asString() ) {
+            int driverCanId = ids.get(i).asInt() - 100;
+            iCanBusSharer[ idxFromCanId[driverCanId] ]->setIEncodersTimedRawExternal( iEncodersTimedRaw[i] );
+        }
+
         //-- Aditionally sets initial parameters on physical motor drivers.
         iCanBusSharer[i]->setCanBusPtr( iCanBus );
     }
