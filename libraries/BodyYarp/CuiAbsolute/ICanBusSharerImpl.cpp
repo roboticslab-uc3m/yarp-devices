0d6550a440dd3a2313125834f727191885c2e6a4
jgvictores <jcgvicto@gmail.com>
2015-07-09 13:07:43 +0200

    cuiabsolute interpretmessage

--- libraries/BodyYarp/CuiAbsolute/ICanBusSharerImpl.cpp
+++ libraries/BodyYarp/CuiAbsolute/ICanBusSharerImpl.cpp
@@ -50,6 +50,11 @@ bool teo::CuiAbsolute::recoverFromError() {
 
 bool teo::CuiAbsolute::interpretMessage( can_msg * message) {
 
+    CD_DEBUG("Got absolute encoder value. %s\n",msgToStr(message).c_str());
+    int got;
+    memcpy(&got, message->data+4,4);
+    CD_SUCCESS("Got absolute encoder value, as an int: %d\n",got);
+
     return true;
 
 }  //-- ends interpretMessage
