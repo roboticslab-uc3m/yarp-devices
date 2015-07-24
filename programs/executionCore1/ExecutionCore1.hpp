d630463420339f74449d82b7c975d648b8d63408
jgvictores <jcgvicto@gmail.com>
2015-07-24 11:50:51 +0200

    header protectors

--- programs/executionCore1/ExecutionCore1.hpp
+++ programs/executionCore1/ExecutionCore1.hpp
@@ -1,7 +1,7 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
-#ifndef __EC1_HPP__
-#define __EC1_HPP__
+#ifndef __EXECUTION_CORE1_HPP__
+#define __EXECUTION_CORE1_HPP__
 
 #include <yarp/os/all.h>
 #include <yarp/dev/all.h>
@@ -96,4 +96,4 @@ private:
 
 }  // namespace teo
 
-#endif  // __EC1_HPP__
+#endif  // __EXECUTION_CORE1_HPP__
