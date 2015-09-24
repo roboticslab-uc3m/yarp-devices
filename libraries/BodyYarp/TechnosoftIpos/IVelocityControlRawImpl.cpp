4137af8222696ff83cb63345fb2e06c9c5903561
jgvictores <jcgvicto@gmail.com>
2015-09-24 11:51:32 +0200

    pass decimal values to fix32 in set value commands

--- libraries/BodyYarp/TechnosoftIpos/IVelocityControlRawImpl.cpp
+++ libraries/BodyYarp/TechnosoftIpos/IVelocityControlRawImpl.cpp
@@ -13,8 +13,11 @@ bool teo::TechnosoftIpos::velocityMoveRaw(int j, double sp) {
     //*************************************************************
     uint8_t msg_vel[]={0x23,0xFF,0x60,0x00,0x00,0x00,0x00,0x00}; // Velocity target
 
-    int16_t sendVel = sp * this->tr / 22.5;  // Apply tr & convert units to encoder increments
-    memcpy(msg_vel+6,&sendVel,2);
+    //int16_t sendVel = sp * this->tr / 22.5;  // Apply tr & convert units to encoder increments
+    //memcpy(msg_vel+6,&sendVel,2);
+    float sendVel = sp * this->tr / 22.5;  // Apply tr & convert units to encoder increments
+    int16_t sendVelFormated = roundf(sendVel * 65536);  // 65536 = 2^16
+    memcpy(msg_vel+4,&sendVelFormated,4);
 
     if( ! send(0x600, 8, msg_vel)){
         CD_ERROR("Sent \"velocity target\". %s\n", msgToStr(0x600, 8, msg_vel).c_str() );
