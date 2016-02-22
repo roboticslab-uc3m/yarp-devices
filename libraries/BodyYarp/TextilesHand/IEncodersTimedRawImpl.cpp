e2fbf55c09680f5c4163d0bec9da6636d003bc45
jgvictores <jcgvicto@gmail.com>
2016-02-22 17:51:26 +0100

    add TextilesHand as copy of LacqueyFetch for now

---
+++ libraries/BodyYarp/TextilesHand/IEncodersTimedRawImpl.cpp
@@ -0,0 +1,21 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "TextilesHand.hpp"
+
+// ------------------ IEncodersTimedRaw Related -----------------------------------------
+
+bool teo::TextilesHand::getEncoderTimedRaw(int j, double *encs, double *time) {
+    //CD_INFO("(%d)\n",j);  //-- Too verbose in controlboardwrapper2 stream.
+
+    //-- Check index within range
+    if ( j != 0 ) return false;
+
+    encoderReady.wait();
+    *encs = encoder;
+    *time = encoderTimestamp;
+    encoderReady.post();
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
