d65149a383fc0b61a04eb58a66eaa17ee666a6a6
jgvictores <jcgvicto@gmail.com>
2016-02-24 11:31:23 +0100

    [CuiAbsoute] almost same code as last; but more general, understandable and commented

--- libraries/BodyYarp/CuiAbsolute/ICanBusSharerImpl.cpp
+++ libraries/BodyYarp/CuiAbsolute/ICanBusSharerImpl.cpp
@@ -51,19 +51,23 @@ bool teo::CuiAbsolute::recoverFromError() {
 bool teo::CuiAbsolute::interpretMessage( can_msg * message) {
 
     //CD_DEBUG("Got absolute encoder value. %s\n",msgToStr(message).c_str());
+
+    //-- Reserve space to copy data from CAN message that contains absolute encoder value
     float got;
+
+    //-- Copy data from CAN message that contains absolute encoder value into "got"
     memcpy(&got, message->data,4);
+
     //CD_SUCCESS("Got absolute encoder value, as a float: %f\n",got);
 
     encoderReady.wait();
 
-    encoder = got * this->tr;
+    //-- "got" is (0,360)degree,, move to (-180,180)degree range
+    if (got > 180.0)
+        got -= 360.0;
 
-    if (encoder < -180.0)
-        encoder += 360.0;
-
-    if (encoder > 180.0)
-        encoder -= 360.0;
+    //-- final used value (encoder) will be this value scaled by tr (usually just 1, or -1 to flip)
+    encoder = got * this->tr;  //-- Even flipping, (-180,180)degree range will become (180,-180)degree range which is safe
 
     encoderTimestamp = message->ts;
 
