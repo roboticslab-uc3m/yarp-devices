96dabadfb216b882466ec7e1ab21a7d8e31ba667
jgvictores <jcgvicto@gmail.com>
2016-03-14 19:06:34 +0100

    init Jr3

---
+++ libraries/BodyYarp/Jr3/Jr3.hpp
@@ -0,0 +1,56 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __JR3__
+#define __JR3__
+
+#include <yarp/os/all.h>
+#include <yarp/dev/all.h>
+#include <sstream>
+
+//#define CD_FULL_FILE  //-- Can be globally managed from father CMake. Good for debugging with polymorphism.
+//#define CD_HIDE_DEBUG  //-- Can be globally managed from father CMake.
+//#define CD_HIDE_SUCCESS  //-- Can be globally managed from father CMake.
+//#define CD_HIDE_INFO  //-- Can be globally managed from father CMake.
+//#define CD_HIDE_WARNING  //-- Can be globally managed from father CMake.
+//#define CD_HIDE_ERROR  //-- Can be globally managed from father CMake.
+#include "ColorDebug.hpp"
+#include "ICanBusSharer.h"
+
+
+namespace teo
+{
+
+/**
+ * @ingroup BodyYarp
+ * \defgroup Jr3
+ * @brief Contains teo::Jr3.
+ */
+
+ /**
+ * @ingroup Jr3
+ * @brief Implementation for the JR3 sensor.
+ *
+ */
+class Jr3 : public yarp::dev::DeviceDriver
+{
+
+    public:
+
+        Jr3() {
+        }
+
+        //  --------- DeviceDriver Declarations. Implementation in Jr3.cpp ---------
+        virtual bool open(yarp::os::Searchable& config);
+        virtual bool close();
+
+        //  --------- ICanBusSharer Declarations. Implementation in Jr3.cpp ---------
+
+
+    protected:
+
+};
+
+}  // namespace teo
+
+#endif  // __JR3__
+
