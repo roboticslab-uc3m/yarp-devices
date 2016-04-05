91323951c6e851e5d20f997d319a3eb5af557b45
rasantos <rasantos@it.uc3m.es>
2016-04-05 17:21:45 +0200

    segfaults

--- test/testTechnosoftIpos.cpp
+++ test/testTechnosoftIpos.cpp
@@ -66,5 +66,24 @@ TEST_F( TechnosoftIposTest, TechnosoftIposOpenCanNode) // -- we call the class t
     ASSERT_EQ(ok, true);    // -- we run the first test
 }
 
+TEST_F( TechnosoftIposTest, TechnosoftIposGetPresence) // -- we call the class that we want to do the test and we assign it a name
+{
+    struct can_msg buffer;
+
+    int canId = 0;
+    int ret = 0;
+    //-- Blocking read until we get a message from the expected canId
+    while ( canId != 15 )
+    {
+        while ( ret <= 0 )
+        {
+            //ret = iCanBus->read_timeout(&buffer,1);
+        }
+        canId = buffer.id  & 0x7F;
+    }
+    //-- Assert the message is of "indicating presence" type.
+    ASSERT_EQ(buffer.id-canId , 0x700);
+}
+
 }  // namespace teo
 
