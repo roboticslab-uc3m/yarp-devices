6bad0bae6545a62705c66977f61fb4ba8ef6d6ff
rasantos <rasantos@it.uc3m.es>
2016-05-17 16:52:08 +0200

    test of sending messages to PIC

--- programs/checkCanBus/CheckCanBus.cpp
+++ programs/checkCanBus/CheckCanBus.cpp
@@ -129,10 +129,10 @@ bool CheckCanBus::configure(yarp::os::ResourceFinder &rf) {
     // -- bool teo::CuiAbsolute::sendDataToPic(uint32_t cob, uint16_t len, uint8_t *msgData)
     if(rf.check("startPicPublishing")){
         printf("[INFO] Start PIC publishing messages\n");
-        uint8_t msgData[]={0x01}; // -- valor de Start que nos hemos inventado para que el PIC comience a publicar
+        uint8_t msgData[3] = {0x01, 0x01, 0}; // -- Comienza a publicar mensajes en modo permanente sin delay
         // -- publishing PIC Cui messages after delay (1s)
         yarp::os::Time::delay(1);
-        cuiAbsoluteEncoder->sendDataToPic(124, 1, msgData);
+        cuiAbsoluteEncoder->sendDataToPic(124, 3, msgData);
     }
 
     // -- Parametro: --cleaningTime [s]
