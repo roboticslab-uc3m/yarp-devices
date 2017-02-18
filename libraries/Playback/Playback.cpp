eea2e3e9b2536b38465f36228fcabd3b5adb5837
jgvictores <jcgvicto@gmail.com>
2017-02-18 21:36:52 +0100

    join Playback impl files

--- libraries/Playback/Playback.cpp
+++ libraries/Playback/Playback.cpp
@@ -42,4 +42,47 @@ bool Playback::close()
 
 // -----------------------------------------------------------------------------
 
+bool Playback::getNumRows(int* num)
+{
+    *num = doublesOnFile.size();
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool Playback::getNext(std::vector<double>& row)
+{
+    if( doublesOnFileIter >= doublesOnFile.size() )
+        return false;
+
+    row = doublesOnFile[doublesOnFileIter];
+
+    doublesOnFileIter++;
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool Playback::parseFileLine(std::vector<double>& doublesOnFileLine)
+{
+    doublesOnFileLine.clear();
+
+    if( file.eof() )
+        return false;
+
+    std::string s;
+    getline(file, s);
+    std::stringstream ss(s);
+
+    double d;
+    while (ss >> d)
+        doublesOnFileLine.push_back(d);
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
 }  // namespace teo
