7886013d4a68adc2b5c7a8083f145351c06658c0
jgvictores <jcgvicto@gmail.com>
2017-02-18 18:40:07 +0100

    add python/exampleRemoteJr3.py

---
+++ example/python/exampleRemoteJr3.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+import yarp
+
+yarp.Network.init()
+
+if yarp.Network.checkNetwork() != True:
+    print '[error] Please try running yarp server'
+    quit()
+
+options = yarp.Property()
+options.put('device','analogsensorclient')
+options.put('remote','/jr3/ch0:o')
+options.put('local','/jr3/ch0:i')
+dd = yarp.PolyDriver(options)  # calls open -> connects
+
+if not dd.isValid():
+    print 'Cannot open the device!'
+    sys.exit()
+
+#pos = dd.viewIPositionControl()
+
+print 'delay(1)'
+yarp.Time.delay(1)
+
+print 'bye!'
+
+
