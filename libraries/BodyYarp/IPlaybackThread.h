08515395eb1c43de6f8ebb8b57f430ce8ad7385d
jgvictores <jcgvicto@gmail.com>
2017-02-20 00:40:53 +0100

    add testPlaybackThread

---
+++ libraries/BodyYarp/IPlaybackThread.h
@@ -0,0 +1,30 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __I_PLAYBACK_THREAD__
+#define __I_PLAYBACK_THREAD__
+
+#include <vector>
+
+namespace teo
+{
+
+/**
+ *
+ * @brief Abstract base for PlaybackThread.
+ *
+ */
+class IPlaybackThread
+{
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~IPlaybackThread() {}
+
+    virtual bool play() = 0;
+
+};
+
+}  // namespace teo
+
+#endif  //  __I_PLAYBACK_THREAD__
---
+++ tests/testPlaybackThread/testPlaybackThread.cpp
@@ -0,0 +1,65 @@
+#include "gtest/gtest.h" // -- We load the librarie of GoogleTest
+
+// -- We load the rest of libraries that we will use to call the functions of our code
+#include <yarp/os/all.h>
+#include <yarp/dev/all.h>
+
+#include "ColorDebug.hpp"
+
+#include "IPlaybackThread.h"
+
+namespace teo
+{
+
+/**
+* @brief Tests \ref Playback
+*/
+class PlaybackThreadTest : public testing::Test // -- inherit the Test class (gtest.h)
+{
+
+public:
+
+    virtual void SetUp()
+    {
+
+        // -- code here will execute just before the test ensues
+
+        yarp::os::Property playbackThreadConf ("(device PlaybackThread) (file test.txt)");
+        bool ok = true;
+        ok &= playbackDevice.open(playbackThreadConf);
+        //ok &= playbackDevice.view(iPlaybackThread);
+
+
+        if(ok)
+        {
+            CD_SUCCESS("Configuration sucessful :)\n");
+        }
+        else
+        {
+            CD_ERROR("Bad Configuration\n");
+            ::exit(1);
+        }
+
+    }
+
+    virtual void TearDown()
+    {
+        // -- code here will be called just after the test completes
+        // -- ok to through exceptions from here if need be
+        playbackDevice.close();
+    }
+
+protected:
+
+    /** Playback device. */
+    yarp::dev::PolyDriver playbackDevice;
+    teo::IPlaybackThread* iPlaybackThread;
+
+};
+
+TEST_F( PlaybackThreadTest, PlaybackThreadTestPlay )
+{
+
+}
+
+}
