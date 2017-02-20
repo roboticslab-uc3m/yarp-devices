b76ad5a603370015783491f27fb9966985e8ffaf
jgvictores <jcgvicto@gmail.com>
2017-02-20 01:15:05 +0100

    implement play, call from test

--- libraries/BodyYarp/PlaybackThread/IPlaybackThreadImpl.cpp
+++ libraries/BodyYarp/PlaybackThread/IPlaybackThreadImpl.cpp
@@ -8,6 +8,21 @@ namespace teo {
 
 bool PlaybackThread::play()
 {
+    std::vector<double> row;
+
+    int rowCounter = 0;
+    while( this->getNext(row) )
+    {
+        std::cout << "Row[" << rowCounter << "]: ";
+        for(int i=0;i<row.size();i++)
+        {
+            std::cout << row[i] << " ";
+        }
+        std::cout << std::endl;
+        yarp::os::Time::delay(row[0]);
+        rowCounter++;
+    }
+
     return true;
 }
 
--- tests/testPlaybackThread/testPlaybackThread.cpp
+++ tests/testPlaybackThread/testPlaybackThread.cpp
@@ -57,7 +57,7 @@ protected:
 
 TEST_F( PlaybackThreadTest, PlaybackThreadTestPlay )
 {
-
+    iPlaybackThread->play();
 }
 
 }
