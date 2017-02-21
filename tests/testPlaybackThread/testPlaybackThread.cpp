27a897f61db231ec7cba5fecdfdbf0859f34a466
jgvictores <jcgvicto@gmail.com>
2017-02-21 13:06:21 +0100

    separate options in lines

--- tests/testPlaybackThread/testPlaybackThread.cpp
+++ tests/testPlaybackThread/testPlaybackThread.cpp
@@ -24,7 +24,9 @@ public:
 
         // -- code here will execute just before the test ensues
 
-        yarp::os::Property playbackThreadConf ("(device PlaybackThread) (file /usr/local/share/teo-body/contexts/Playback/txt/testPlayback.txt)");
+        yarp::os::Property playbackThreadConf;
+        playbackThreadConf.put("device","PlaybackThread");
+        playbackThreadConf.put("file","/usr/local/share/teo-body/contexts/Playback/txt/testPlayback.txt)");
         bool ok = true;
         ok &= playbackDevice.open(playbackThreadConf);
         ok &= playbackDevice.view(iPlaybackThread);
