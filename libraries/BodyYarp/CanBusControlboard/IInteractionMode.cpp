a269868aeedf19cf158e6f1ce16f92c624f5295f
rsantos88 <rasantos@it.uc3m.es>
2017-02-28 16:56:21 +0100

    implemented functiones of IInteractionMode

--- libraries/BodyYarp/CanBusControlboard/IInteractionMode.cpp
+++ libraries/BodyYarp/CanBusControlboard/IInteractionMode.cpp
@@ -7,30 +7,71 @@
 
 bool teo::CanBusControlboard::getInteractionMode(int axis, yarp::dev::InteractionModeEnum* mode)
 {
+    CD_INFO("(%d)\n",axis);
+
+    *mode = interactionMode[axis];
     return true;
 }
 
 bool teo::CanBusControlboard::getInteractionModes(int n_joints, int *joints, yarp::dev::InteractionModeEnum* modes)
 {
-    return true;
+    CD_INFO("\n");
+
+        bool ok = true;
+        for(unsigned int i=0; i < nodes.size(); i++)
+        {
+            if( joints[i] )
+            {
+                ok &= getInteractionMode(i,&modes[i]);
+            }
+        }
+        return ok;
 }
 
 bool teo::CanBusControlboard::getInteractionModes(yarp::dev::InteractionModeEnum* modes)
 {
-    return true;
+    CD_INFO("\n");
+
+    bool ok = true;
+    for(unsigned int i=0; i < nodes.size(); i++)
+        ok &= getInteractionMode(i,&modes[i]);
+    return ok;
 }
 
 bool teo::CanBusControlboard::setInteractionMode(int axis, yarp::dev::InteractionModeEnum mode)
 {
+    CD_INFO("(%d)\n",axis);
+
+    interactionModeSemaphore.wait();
+    interactionMode[axis] = mode;
+    interactionModeSemaphore.post();
+
     return true;
 }
 
 bool teo::CanBusControlboard::setInteractionModes(int n_joints, int *joints, yarp::dev::InteractionModeEnum* modes)
 {
-    return true;
+    CD_INFO("\n");
+
+       bool ok = true;
+       for(int j=0; j<nodes.size(); j++)
+       {
+           if( joints[j] )
+           {
+               ok &= this->setInteractionMode(j,modes[j]);
+           }
+       }
+       return ok;
 }
 
 bool teo::CanBusControlboard::setInteractionModes(yarp::dev::InteractionModeEnum* modes)
 {
-    return true;
+    CD_INFO("\n");
+
+    bool ok = true;
+    for(unsigned int i=0; i<nodes.size(); i++)
+    {
+        ok &= setInteractionMode(i,modes[i]);
+    }
+    return ok;
 }
