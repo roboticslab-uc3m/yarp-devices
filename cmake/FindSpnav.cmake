5ed99108eeb9434217dd53463bfe52d79295104a
Bartek Łukawski <bwmn.peter@gmail.com>
2017-03-24 13:53:45 +0100

    Pull up FindPackageHandleStandardArgs bits in FindSpnav.cmake

--- cmake/FindSpnav.cmake
+++ cmake/FindSpnav.cmake
@@ -7,14 +7,19 @@
 # SPNAV_LIBRARIES    - Spnav libraries
 
 if(UNIX)
-    find_path(SPNAV_INCLUDE_DIR spnav.h)
-    find_library(SPNAV_LIBRARY NAMES spnav libspnav)
+    if(NOT SPNAV_INCLUDE_DIR)
+        find_path(SPNAV_INCLUDE_DIR spnav.h)
+    endif()
 
-    include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(Spnav DEFAULT_MSG SPNAV_INCLUDE_DIR SPNAV_LIBRARY)
-
-    mark_as_advanced(SPNAV_INCLUDE_DIR SPNAV_LIBRARY)
-
-    set(SPNAV_INCLUDE_DIRS ${SPNAV_INCLUDE_DIR})
-    set(SPNAV_LIBRARIES ${SPNAV_LIBRARY})
+    if(NOT SPNAV_LIBRARY)
+        find_library(SPNAV_LIBRARY NAMES spnav libspnav)
+    endif()
 endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Spnav DEFAULT_MSG SPNAV_INCLUDE_DIR SPNAV_LIBRARY)
+
+mark_as_advanced(SPNAV_INCLUDE_DIR SPNAV_LIBRARY)
+
+set(SPNAV_INCLUDE_DIRS ${SPNAV_INCLUDE_DIR})
+set(SPNAV_LIBRARIES ${SPNAV_LIBRARY})
