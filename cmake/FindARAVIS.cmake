824ff3b6fcdf84975c0d4098908a5e43f781bf61
David-Estevez <david.estevez.fdez@gmail.com>
2017-03-31 16:46:25 +0200

    Add GLib as Aravis dependency

--- cmake/FindARAVIS.cmake
+++ cmake/FindARAVIS.cmake
@@ -5,6 +5,12 @@
 #  ARAVIS_LIBRARIES - The libraries needed to use Aravis
 #  ARAVIS_DEFINITIONS - Compiler switches required for using Aravis
 
+find_package(GLib REQUIRED)
+
+message("--->")
+message(${GLib_INCLUDE_DIRS})
+message("--->")
+
 find_path(ARAVIS_INCLUDE_DIR arv.h
   $ENV{ARAVIS_DIR}
   /usr/local/include/aravis-0.4
@@ -15,5 +21,6 @@ find_library(ARAVIS_LIBRARY NAMES aravis-0.4 libaravis-0.4 )
 
 mark_as_advanced(ARAVIS_INCLUDE_DIR ARAVIS_LIBRARY )
 
-set(ARAVIS_INCLUDE_DIRS ${ARAVIS_INCLUDE_DIR} )
-set(ARAVIS_LIBRARIES ${ARAVIS_LIBRARY} )
+set(ARAVIS_INCLUDE_DIRS ${ARAVIS_INCLUDE_DIR} ${GLib_INCLUDE_DIRS})
+#set(ARAVIS_LINK_DIRS ${GLib_LIBDIR} )
+set(ARAVIS_LIBRARIES ${ARAVIS_LIBRARY} ${GLib_LIBRARY})
