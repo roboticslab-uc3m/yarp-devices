0c5b7448f8b5bb9e661136cd483c1f33fc41c299
rsantos88 <rasantos@it.uc3m.es>
2017-03-31 10:47:03 +0200

    separating functions looks more clear and now CD_ERROR shows Missing implementation

--- libraries/BodyYarp/CuiAbsolute/IVelocityControl2RawImpl.cpp
+++ libraries/BodyYarp/CuiAbsolute/IVelocityControl2RawImpl.cpp
@@ -2,72 +2,90 @@
 
 #include "CuiAbsolute.hpp"
 
-//  --------- IVelocityControlRaw implementations ---------
+//  ########################### IVelocityControlRaw implementations ###########################
 
 bool teo::CuiAbsolute::setVelocityModeRaw()
 {
-    CD_ERROR("\n");
+    CD_ERROR("Missing implementation\n");
     return false;
 }
+
+// --------------------------------------------------------------------------------------------
 
 bool teo::CuiAbsolute::velocityMoveRaw(int j, double sp)
 {
-    CD_INFO("\n");
+    CD_INFO("Missing implementation\n");
     return true;
 }
 
+// --------------------------------------------------------------------------------------------
+
 bool teo::CuiAbsolute::velocityMoveRaw(const double *sp)
 {
-    CD_ERROR("\n");
+    CD_ERROR("Missing implementation\n");
     return false;
 }
 
-//  --------- IVelocityControl2Raw implementations ----------
+//  ########################### IVelocityControl2Raw implementations ###########################
 
 bool teo::CuiAbsolute::velocityMoveRaw(const int n_joint, const int *joints, const double *spds)
 {
-    CD_ERROR("\n");
+    CD_ERROR("Missing implementation\n");
     return false;
 }
+
+// --------------------------------------------------------------------------------------------
 
 bool teo::CuiAbsolute::getRefVelocityRaw(const int joint, double *vel)
 {
-    CD_ERROR("\n");
+    CD_ERROR("Missing implementation\n");
     return false;
 }
+
+// --------------------------------------------------------------------------------------------
 
 bool teo::CuiAbsolute::getRefVelocitiesRaw(double *vels)
 {
-    CD_ERROR("\n");
+    CD_ERROR("Missing implementation\n");
     return false;
 }
+
+// --------------------------------------------------------------------------------------------
 
 bool teo::CuiAbsolute::getRefVelocitiesRaw(const int n_joint, const int *joints, double *vels)
 {
-    CD_ERROR("\n");
+    CD_ERROR("Missing implementation\n");
     return false;
 }
+
+// --------------------------------------------------------------------------------------------
 
 bool teo::CuiAbsolute::setVelPidRaw(int j, const yarp::dev::Pid &pid)
 {
-    CD_ERROR("\n");
+    CD_ERROR("Missing implementation\n");
     return false;
 }
+
+// --------------------------------------------------------------------------------------------
 
 bool teo::CuiAbsolute::setVelPidsRaw(const yarp::dev::Pid *pids)
 {
-    CD_ERROR("\n");
+    CD_ERROR("Missing implementation\n");
     return false;
 }
+
+// --------------------------------------------------------------------------------------------
 
 bool teo::CuiAbsolute::getVelPidRaw(int j, yarp::dev::Pid *pid)
 {
-    CD_ERROR("\n");
+    CD_ERROR("Missing implementation\n");
     return false;
 }
 
+// --------------------------------------------------------------------------------------------
+
 bool teo::CuiAbsolute::getVelPidsRaw(yarp::dev::Pid *pids)
 {
-    CD_ERROR("\n");
+    CD_ERROR("Missing implementation\n");
     return false;
 }
