ae6a2c68ed6162611caa7b00f7a5d043cd9f5bd2
rsantos88 <rasantos@it.uc3m.es>
2017-03-31 11:54:05 +0200

    separating functions looks more clear

--- libraries/BodyYarp/FakeJoint/IPositionDirectRawImpl.cpp
+++ libraries/BodyYarp/FakeJoint/IPositionDirectRawImpl.cpp
@@ -10,12 +10,16 @@ bool teo::FakeJoint::setPositionDirectModeRaw()
     return true;
 }
 
+// ------------------------------------------------------------------------------
+
 bool teo::FakeJoint::setPositionRaw(int j, double ref)
 {
     CD_DEBUG("\n");
     this->positionMoveRaw(0,ref);
     return true;
 }
+
+// ------------------------------------------------------------------------------
 
 bool teo::FakeJoint::setPositionsRaw(const int n_joint, const int *joints, double *refs)
 {
@@ -24,8 +28,12 @@ bool teo::FakeJoint::setPositionsRaw(const int n_joint, const int *joints, doubl
     return true;
 }
 
+// ------------------------------------------------------------------------------
+
 bool teo::FakeJoint::setPositionsRaw(const double *refs)
 {
     CD_DEBUG("\n");
     return true;
 }
+
+// ------------------------------------------------------------------------------
