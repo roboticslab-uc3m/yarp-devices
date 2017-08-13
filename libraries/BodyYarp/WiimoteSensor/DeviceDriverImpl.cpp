35f30221c5bd8a9fdf5ddfe1cb84d8b7f636aa70
Bartek Łukawski <bwmn.peter@gmail.com>
2017-08-13 21:52:23 +0200

    Deal with device initialization and destruction

--- libraries/BodyYarp/WiimoteSensor/DeviceDriverImpl.cpp
+++ libraries/BodyYarp/WiimoteSensor/DeviceDriverImpl.cpp
@@ -2,10 +2,45 @@
 
 #include "WiimoteSensor.hpp"
 
+#include <cstdlib>
+
+#include <yarp/os/Value.h>
+
+#include "ColorDebug.hpp"
+
 // -----------------------------------------------------------------------------
 
 bool roboticslab::WiimoteSensor::open(yarp::os::Searchable& config)
 {
+    int deviceId = config.check("deviceId", yarp::os::Value(DEFAULT_DEVICE), "Wiimote device number").asInt();
+
+    char * syspath = getDevicePath(deviceId);
+
+    if (syspath == NULL)
+    {
+        CD_ERROR("Cannot find device with number %d.\n", deviceId);
+        return false;
+    }
+
+    int ret = xwii_iface_new(&iface, syspath);
+
+    std::free(syspath);
+
+    if (ret < 0)
+    {
+        CD_ERROR("Cannot create xwii_iface '%s' (error: %d).\n", syspath, ret);
+        return false;
+    }
+
+    ret = xwii_iface_open(iface, XWII_IFACE_CORE | XWII_IFACE_ACCEL);
+
+    if (ret < 0)
+    {
+        CD_ERROR("Cannot open interface: %d.\n", ret);
+        close();
+        return false;
+    }
+
     return true;
 }
 
@@ -13,6 +48,7 @@ bool roboticslab::WiimoteSensor::open(yarp::os::Searchable& config)
 
 bool roboticslab::WiimoteSensor::close()
 {
+    xwii_iface_unref(iface);
     return true;
 }
 
--- libraries/BodyYarp/WiimoteSensor/WiimoteSensor.hpp
+++ libraries/BodyYarp/WiimoteSensor/WiimoteSensor.hpp
@@ -8,6 +8,8 @@
 
 #include "ColorDebug.hpp"
 
+#define DEFAULT_DEVICE 1
+
 namespace roboticslab
 {
 
@@ -23,62 +25,68 @@ namespace roboticslab
  */
 class WiimoteSensor : public yarp::dev::DeviceDriver, public yarp::dev::IAnalogSensor
 {
-    public:
+public:
 
-        WiimoteSensor();
+    WiimoteSensor();
 
-        //  --------- DeviceDriver Declarations. Implementation in DeviceDriverImpl.cpp ---------
-        virtual bool open(yarp::os::Searchable& config);
-        virtual bool close();
+    //  --------- DeviceDriver Declarations. Implementation in DeviceDriverImpl.cpp ---------
+    virtual bool open(yarp::os::Searchable& config);
+    virtual bool close();
 
-        //  --------- IAnalogSensor Declarations. Implementation in IAnalogSensorImpl.cpp ---------
-        /**
-         * Read a vector from the sensor.
-         * @param out a vector containing the sensor's last readings.
-         * @return AS_OK or return code. AS_TIMEOUT if the sensor timed-out.
-         */
-        virtual int read(yarp::sig::Vector &out);
+    //  --------- IAnalogSensor Declarations. Implementation in IAnalogSensorImpl.cpp ---------
+    /**
+     * Read a vector from the sensor.
+     * @param out a vector containing the sensor's last readings.
+     * @return AS_OK or return code. AS_TIMEOUT if the sensor timed-out.
+     */
+    virtual int read(yarp::sig::Vector &out);
 
-        /**
-         * Check the state value of a given channel.
-         * @param ch channel number.
-         * @return status.
-         */
-        virtual int getState(int ch);
+    /**
+     * Check the state value of a given channel.
+     * @param ch channel number.
+     * @return status.
+     */
+    virtual int getState(int ch);
 
-        /**
-         * Get the number of channels of the sensor.
-         * @return number of channels (0 in case of errors).
-         */
-        virtual int getChannels();
+    /**
+     * Get the number of channels of the sensor.
+     * @return number of channels (0 in case of errors).
+     */
+    virtual int getChannels();
 
-        /**
-         * Calibrates the whole sensor.
-         * @return status.
-         */
-        virtual int calibrateSensor();
+    /**
+     * Calibrates the whole sensor.
+     * @return status.
+     */
+    virtual int calibrateSensor();
 
-        /**
-         * Calibrates the whole sensor, using an vector of calibration values.
-         * @param value a vector of calibration values.
-         * @return status.
-         */
-        virtual int calibrateSensor(const yarp::sig::Vector& value);
+    /**
+     * Calibrates the whole sensor, using an vector of calibration values.
+     * @param value a vector of calibration values.
+     * @return status.
+     */
+    virtual int calibrateSensor(const yarp::sig::Vector& value);
 
-        /**
-         * Calibrates one single channel.
-         * @param ch channel number.
-         * @return status.
-         */
-        virtual int calibrateChannel(int ch);
+    /**
+     * Calibrates one single channel.
+     * @param ch channel number.
+     * @return status.
+     */
+    virtual int calibrateChannel(int ch);
 
-        /**
-         * Calibrates one single channel, using a calibration value.
-         * @param ch channel number.
-         * @param value calibration value.
-         * @return status.
-         */
-        virtual int calibrateChannel(int ch, double value);
+    /**
+     * Calibrates one single channel, using a calibration value.
+     * @param ch channel number.
+     * @param value calibration value.
+     * @return status.
+     */
+    virtual int calibrateChannel(int ch, double value);
+
+private:
+
+    static char * getDevicePath(int id);
+
+    struct xwii_iface * iface;
 };
 
 }  // namespace roboticslab
