5540095acb64c6e7f9c7e89e35f4b0a743a29b9e
David-Estevez <david.estevez.fdez@gmail.com>
2017-11-30 17:19:51 +0100

    Add simple example to test python yarp bindings with AravisGigE camera

--- programs/AravisGigEController/AravisGigEController.py
+++ programs/AravisGigEController/AravisGigEController.py
@@ -1,3 +1,36 @@
 #!/usr/bin/env python3
 
-print("Hello world!!")
+"""
+AravisGigEController
+---------------------
+
+Simple GUI for controlling GigE cameras using Aravis and YARP
+
+Author: David Estevez
+Copyright: Universidad Carlos III de Madrid (C) 2017;
+CopyPolicy: Released under the terms of the GNU GPL v2.0.
+"""
+
+import logging
+
+import yarp
+import begin
+
+
+@begin.start(auto_convert=True, config_file='config.txt')
+@begin.logging
+def main(remote_port: 'Remote port running the AravisGigE grabber'='/grabber'):
+
+    # Check for YARP network
+    yarp.Network.init()
+    if not yarp.Network.checkNetwork():
+        logging.error('Could not connect to YARP network. Please try running YARP server.')
+        quit()
+
+    # Create and configure driver
+    options = yarp.Property()
+    options.put('device','remote_controlboard')
+    options.put('remote', remote_port)
+    options.put('local','/AravisGigEController')
+    dd = yarp.PolyDriver(options)
+
