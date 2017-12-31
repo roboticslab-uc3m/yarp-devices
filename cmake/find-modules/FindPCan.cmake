ad3d41a67c27571c3ac4807dfde7377035a42527
Bartek Łukawski <bwmn.peter@gmail.com>
2017-12-31 11:30:22 +0100

    Create CMake targets for PCan and use them

--- cmake/find-modules/FindPCan.cmake
+++ cmake/find-modules/FindPCan.cmake
@@ -5,6 +5,11 @@
 # PCAN_FOUND        - system has PCan
 # PCan_INCLUDE_DIRS - PCan include directories
 # PCan_LIBRARIES    - PCan libraries
+#
+# ...and the following imported targets (requires CMake 2.8.11+):
+#
+# PCan::PCan        - PCan library (old API)
+# PCan::PCanFD      - PCan library (v8+ API)
 
 if(UNIX)
     find_path(PCan_INCLUDE_DIR NAMES pcan.h
@@ -24,6 +29,22 @@ find_package_handle_standard_args(PCan DEFAULT_MSG PCan_INCLUDE_DIR
 if(PCan_FOUND)
     set(PCan_INCLUDE_DIRS ${PCan_INCLUDE_DIR})
     set(PCan_LIBRARIES ${PCan_LIBRARY} ${PCan_LIBRARY_FD})
+
+    if(NOT CMAKE_VERSION VERSION_LESS 2.8.11)
+        if(NOT TARGET PCan::PCan)
+            add_library(PCan::PCan UNKNOWN IMPORTED)
+
+            set_target_properties(PCan::PCan PROPERTIES IMPORTED_LOCATION "${PCan_LIBRARY}"
+                                                        INTERFACE_INCLUDE_DIRECTORIES "${PCan_INCLUDE_DIR}")
+        endif()
+
+	    if(NOT TARGET PCan::PCanFD)
+            add_library(PCan::PCanFD UNKNOWN IMPORTED)
+
+            set_target_properties(PCan::PCanFD PROPERTIES IMPORTED_LOCATION "${PCan_LIBRARY_FD}"
+                                                          INTERFACE_INCLUDE_DIRECTORIES "${PCan_INCLUDE_DIR}")
+        endif()
+    endif()
 endif()
 
 mark_as_advanced(PCan_INCLUDE_DIR PCan_LIBRARY PCan_LIBRARY_FD)
