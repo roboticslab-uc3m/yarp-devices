1ff173002fe444b43e7a064fda4390f0bd29ca26
jgvictores <jcgvicto@gmail.com>
2018-05-15 18:26:31 +0200

    best tuned values for both FT ankle sensors

--- libraries/YarpPlugins/Jr3/DeviceDriverImpl.cpp
+++ libraries/YarpPlugins/Jr3/DeviceDriverImpl.cpp
@@ -15,7 +15,7 @@ bool roboticslab::Jr3::open(yarp::os::Searchable& config)
     int ret;
 
     //-- Force fullscales.
-    force_array fs_w,fs_a;
+    force_array fs_w, fs_a0, fs_a1;
     fs_w.f[0]=110;
     fs_w.f[1]=110;
     fs_w.f[2]=220;
@@ -23,15 +23,22 @@ bool roboticslab::Jr3::open(yarp::os::Searchable& config)
     fs_w.m[1]=5.5;
     fs_w.m[2]=5.5;
 
-    fs_a.f[0]=317;
-    fs_a.f[1]=314;
-    fs_a.f[2]=845;
-    fs_a.m[0]=221;
-    fs_a.m[1]=219;
-    fs_a.m[2]=307;
+    fs_a0.f[0]=317;
+    fs_a0.f[1]=314;
+    fs_a0.f[2]=845;
+    fs_a0.m[0]=221;
+    fs_a0.m[1]=219;
+    fs_a0.m[2]=307;
 
-    ioctl(fd,IOCTL0_JR3_SET_FULL_SCALES,&fs_a);
-    ioctl(fd,IOCTL1_JR3_SET_FULL_SCALES,&fs_a);
+    fs_a1.f[0]=326;
+    fs_a1.f[1]=325;
+    fs_a1.f[2]=870;
+    fs_a1.m[0]=224;
+    fs_a1.m[1]=224;
+    fs_a1.m[2]=309;
+
+    ioctl(fd,IOCTL0_JR3_SET_FULL_SCALES,&fs_a0);
+    ioctl(fd,IOCTL1_JR3_SET_FULL_SCALES,&fs_a1);
     ioctl(fd,IOCTL2_JR3_SET_FULL_SCALES,&fs_w);
     ioctl(fd,IOCTL3_JR3_SET_FULL_SCALES,&fs_w);
 
@@ -40,7 +47,7 @@ bool roboticslab::Jr3::open(yarp::os::Searchable& config)
 
     //-- Make sure fullscales were set.
     ret=ioctl(fd,IOCTL0_JR3_GET_FULL_SCALES,&fs0);
-    CD_INFO("Full scales of Sensor 0 are %d %d %d %d %d %d\n",fs0.f[0],fs0.f[1],fs0.f[2],fs0.m[0],fs0.m[1],fs0.m[2]);
+    CD_INFO("Full scales of Sensor 0 are: %d %d %d %d %d %d\n", fs0.f[0],fs0.f[1],fs0.f[2],fs0.m[0],fs0.m[1],fs0.m[2]);
     ret=ioctl(fd,IOCTL1_JR3_GET_FULL_SCALES,&fs1);
     CD_INFO("Full scales of Sensor 1 are: %d %d %d %d %d %d\n", fs1.f[0],fs1.f[1],fs1.f[2],fs1.m[0],fs1.m[1],fs1.m[2]);
     ret=ioctl(fd,IOCTL2_JR3_GET_FULL_SCALES,&fs2);
