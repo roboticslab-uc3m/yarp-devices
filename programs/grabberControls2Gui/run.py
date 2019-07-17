27e7d922486ef2cb879c838a919c95f12d84b79d
David-Estevez <david.estevez.fdez@gmail.com>
2019-07-17 17:49:34 +0200

    Import updates from horus, bump PySide2 version

--- programs/grabberControls2Gui/run.py
+++ programs/grabberControls2Gui/run.py
@@ -12,8 +12,10 @@ CopyPolicy: Released under the terms of the GNU GPL v2.0.
 """
 
 import sys
+import logging
 
 import begin
+import yarp
 from PySide2 import QtWidgets
 
 from .GrabberControls2GuiBackend import GrabberControls2GuiBackend
@@ -23,13 +25,33 @@ from .GrabberControls2GuiGUI import GrabberControls2GuiGUI
 @begin.start(auto_convert=True)
 @begin.logging
 def main(remote_port: 'Remote port running the AravisGigE grabber'='/grabber'):
+    # Check for YARP network
+    yarp.Network.init()
+    if not yarp.Network.checkNetwork():
+        logging.error('Could not connect to YARP network. Please try running YARP server.')
+        quit()
+
+    # Create and configure driver
+    options = yarp.Property()
+    options.put('device', 'remote_grabber')
+    options.put('remote', remote_port)
+    options.put('local', '/grabberControls2Gui')
+    dd = yarp.PolyDriver(options)
+
+    # View driver as FrameGrabber
+    controls = dd.viewIFrameGrabberControls2()
+
     # Create Qt app
     app = QtWidgets.QApplication(sys.argv)
 
     # Create the widget and show it
-    controller = GrabberControls2GuiBackend(remote_port)
+    controller = GrabberControls2GuiBackend(controls)
     gui = GrabberControls2GuiGUI(controller)
     gui.show()
 
     # Run the app
-    sys.exit(app.exec_())
+    exit_code = app.exec_()
+
+    dd.close()
+    yarp.Network.fini()  # disconnect from the YARP network
+    sys.exit()
