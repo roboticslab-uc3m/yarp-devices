0e322b17120b5685aa5761073bbba4c4f4a1ef63
Bartek Łukawski <bwmn.peter@gmail.com>
2019-09-13 03:57:45 +0200

    [WIP] Allow non-fundamental types with SFINAE

--- libraries/CanBusSharerLib/StateObserver.hpp
+++ libraries/CanBusSharerLib/StateObserver.hpp
@@ -17,10 +17,20 @@ public:
     StateObserverBase(double timeout);
     virtual ~StateObserverBase() = 0;
 
+    double getTimeout() const
+    { return timeout; }
+
     bool await(void * raw = nullptr);
     bool notify(const void * raw = nullptr, std::size_t len = 0);
 
+protected:
+    void * getRemoteStorage();
+    const void * getRemoteStorage() const;
+    virtual void setRemoteStorage(const void * ptr, std::size_t len);
+
 private:
+    double timeout;
+
     class Private;
     Private * impl;
 };
@@ -41,7 +51,7 @@ public:
     { return StateObserverBase::notify(); }
 };
 
-template<typename T>
+template<typename T, typename NonFundamentalType = void>
 class TypedStateObserver : private StateObserverBase
 {
 public:
@@ -51,13 +61,32 @@ public:
     TypedStateObserver(const TypedStateObserver &) = delete;
     TypedStateObserver & operator=(const TypedStateObserver &) = delete;
 
+    bool await(T * remote)
+    { return StateObserverBase::await(remote); }
+
+    bool notify(const T * remote)
+    { return StateObserverBase::notify(remote); }
+
+protected:
+    virtual void setRemoteStorage(const void * remote, std::size_t len) override
+    { *reinterpret_cast<T *>(getRemoteStorage()) = *reinterpret_cast<const T *>(remote); }
+};
+
+template<typename T>
+class TypedStateObserver<T, std::enable_if<std::is_integral<T>::value>> : private StateObserverBase
+{
+public:
+    TypedStateObserver(double timeout) : StateObserverBase(timeout)
+    { }
+
+    TypedStateObserver(const TypedStateObserver &) = delete;
+    TypedStateObserver & operator=(const TypedStateObserver &) = delete;
+
     bool await(T * raw)
-    { static_assert(std::is_fundamental<T>::value, "Fundamental type is required.");
-      return StateObserverBase::await(raw); }
+    { return StateObserverBase::await(raw); }
 
     bool notify(const T * raw, std::size_t len = sizeof(T))
-    { static_assert(std::is_fundamental<T>::value, "Fundamental type is required.");
-      return StateObserverBase::notify(raw, len); }
+    { return StateObserverBase::notify(raw, len); }
 };
 
 } // namespace roboticslab
