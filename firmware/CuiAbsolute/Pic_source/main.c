cf24aaec2a1575c5835a8d75d4154488e5f4b5e8
rsantos88 <rasantos@it.uc3m.es>
2019-09-20 14:26:08 +0200

    updated the PIC firmware

--- firmware/CuiAbsolute/Pic_source/main.c
+++ firmware/CuiAbsolute/Pic_source/main.c
@@ -5,40 +5,29 @@
  * @ingroup teo_body_cuiAbsolute
  * \defgroup teo_body_picFirmware PIC-Firmware
  *
- * @brief Firmware that allows communication with the PIC and the publication of
-position messages.
+ * @brief Firmware that allows communication with the PIC and the publication of position messages.
  *
  * @section teo_body_firmware_legal Legal
  *
  * Copyright: 2016 (C) Universidad Carlos III de Madrid
  *
- * Author: <a
-href="http://roboticslab.uc3m.es/roboticslab/people/r-de-santos">Raul de Santos
-Rico</a>
+ * Author: <a> href="http://roboticslab.uc3m.es/roboticslab/people/r-de-santos">Raul de Santos Rico</a>
  *
- * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see
-license/LGPL.TXT
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see license/LGPL.TXT
  *
  * @section cuiAbsolute_install Installation
  *
  * You need to install MPLAB IDE v8.92 and MPLAB C Compiler for PIC18 MCUs. <br>
- * Open the proyect with MPLAB, open "main.c" file and change the value of
-"canId" variable (You can see <a class="el"
-href="http://robots.uc3m.es/index.php/CuiAbsolute_Documentation">correspondence</a>)
-<br>
- * Also, to increase the time between sending messages,  you can modify
-"sendDelay" value too (see code documentation in main.c).
+ * Open the proyect with MPLAB, open "main.c" file and change the value of "canId" variable spondence</a>) <br>
+ * Also, to increase the time between sending messages,  you can modify "sendDelay" value too (see code documentation in main.c)
  * Then, build all. <br>
- * Finally, select programmer "MPLAB ICD 2", disconnect CAN-BUS connections of
-the PIC, connect and program it. <br>
+ * Finally, select programmer "MPLAB ICD 2", disconnect CAN-BUS connections of the PIC, connect and program it. <br>
  *
  * @section cuiAbsolute_running Running (assuming correct installation)
  *
  *
- * To test Cui Aboslute encoders are working properly, you need to start the
-program named "testCuiAbsolute" and see that the test passed correctly. <br>
- * Previously, you have to manually change the ID of Cui that you want to test
-in the code (example: #define CAN_ID 124), compile and reinstall. <br>
+ * To test Cui Aboslute encoders are working properly, you need to start the program named "testCuiAbsolute" and see that the test passed correctly. <br>
+ * Previously, you have to manually change the ID of Cui that you want to test in the code (example: #define CAN_ID 124), compile and reinstall. <br>
  * The testCuiAbsolute application can be edited at:
  * tests/testCuiAbsolute.cpp
 \verbatim
@@ -77,28 +66,32 @@ in the code (example: #define CAN_ID 124), compile and reinstall. <br>
 
 // Variable configurable (CAN_ID)
 // -----------------------
-unsigned long canId = 124;
+const unsigned long canId = 124;
 // -----------------------
 
-// -- Inicialización de variables para el envío
+// pulsos-por-slot * encoderPulses (numero-total-de-slots) / 360
+const double div = 4 * 1024 / 360.0; // approx. 11.38
+
+// -- Inicializacion de variables para el envio
+BYTE delay;
 int aux, message[2];
 double degrees;
-double div = 11.38; // resultado dividir (2^12)/360 = 11.38
 BYTE x, y;
 ECAN_TX_MSG_FLAGS txFlags =
     ECAN_TX_PRIORITY_3 & ECAN_TX_STD_FRAME & ECAN_TX_NO_RTR_FRAME;
 
 // int stop_flag=0; // -- flag para saber cuando se ha recibido un stop
-int pushFlag = 0; // flag para saber si está ejecutando el PUSH
+int pushFlag = 0; // flag para saber si esta ejecutando el PUSH
 
-// -- Variables que almacenarán los datos a recibir:
+// -- Variables que almacenaran los datos a recibir:
 unsigned long picId;
 BYTE data[8];
 BYTE dataLen;
 ECAN_RX_MSG_FLAGS rxflags;
 
 // -- Prototipos de funciones
-void send(void);
+void sendData(BYTE);
+void sendAck(BYTE);
 void cleanData(void);
 
 // -- Funcion principal
@@ -121,8 +114,8 @@ void main(void) {
   ECANSetOperationMode(ECAN_OP_MODE_CONFIG);
 
   /* Manual (page 1):
-  ECAN provides three modes of operation – Mode 0, Mode 1 and Mode 2. Mode 0 is fully backward compat-
-  ible with the legacy CAN module. Applications developed for the legacy CAN module would continue to
+  ECAN provides three modes of operation : Mode 0, Mode 1 and Mode 2. Mode 0 is fully backward compat-
+  ible with the legacy CAN module. Applications developed for the legacy CAN  module would continue to
   work without any change using ECAN. Mode 1 is the Enhanced Legacy mode with increased buffers and fil-
   ters. Mode 2 has the same resources as Mode 1, but with a hardware managed receive FIFO. Given its fea-
   tures and flexibility, ECAN would prove useful to many CAN-based applications.
@@ -133,10 +126,8 @@ void main(void) {
   ECANSetRXB0DblBuffer(ECAN_DBL_BUFFER_MODE_DISABLE);
 
   /* Manual (page 44):
-  This macro sets the value for a mask register. There are a total of two
-  macros, one for each mask.
-  For example, for mask RXM0, use ECANSetRXM0Value, for RXM1, use
-  ECANSetRXM1Value and so on
+  This macro sets the value for a mask register. There are a total of two  macros, one for each mask.
+  For example, for mask RXM0, use ECANSetRXM0Value, for RXM1, use  ECANSetRXM1Value and so on
   ECAN_MSG_STD Standard type. 11-bit of value will be used.
   ECAN_MSG_XTD Extended type. 29-bit of value will be used.*/
   ECANSetRXM0Value(-1, ECAN_MSG_STD);
@@ -159,58 +150,62 @@ void main(void) {
 
   /* Mensaje compuesto por 2 bytes
   -- data[0]: (Byte 1)
-      * start push (0x01) publicación permanente (necesita indicar periodo de publicación)
-      * start pull (0x02) publicación por petición (no necesita indicar velocidad de publicación)
+      * start push (0x01) publicacion permanente (necesita indicar periodo de publicacion)
+      * start pull (0x02) publicacion por peticion (no necesita indicar velocidad de publicacion)
       * stop  push (0x03) para la publicación por push
   -- data[1]: (Byte 2)
-      * Periodo de publicación entre mensajes usando publicación tipo push.
+      * Periodo de publicacion entre mensajes usando publicacion tipo push.
   Valores (0 - 255)
       A tener en cuenta:
-              - La frecuencia de oscilación se encuentra definida como Fosc =  5Mhz (20/4)
+              - La frecuencia de oscilación se encuentra definida como Fosc = 5Mhz (20/4)
               - La velocidad de ejecución de cada ciclo de instrucción son 0.8 microsegundos
               - Delay10TCYx(i) -> 10*Tcy*i. Por tanto Delay10TCYc(1) equivale a 8 microsegundos (10 ciclos de reloj)
   */
+
+  delay = 0;
 
   while (1) {
     // --------------- Recibo!!! ------------------
     BOOL ret = ECANReceiveMessage(&picId, data, &dataLen, &rxflags);
 
-    // if (ret = 0) && (pushFlag = 0)
-    if (!ret && !pushFlag)
-      continue;
+    if (!ret || picId != canId) {
+      if (pushFlag) {
+        sendData(0x80);
+        Delay1KTCYx(delay);
+      }
 
-    if (picId != canId)
       continue;
+    }
 
     switch (data[0]) {
-    case 0x01: // start push (continuous)
-      send();
-      Delay100TCYx((unsigned char)data[1]);
+    case 0x01: // start push (continuous mode)
+      delay = data[1];
       pushFlag = 1;
+      sendAck(0x100);
       break;
 
-    case 0x02: // start pull (polling)
-      send();
-      cleanData();
-      break;
-
-    case 0x03: // (case 0x03)stop
-      cleanData();
+    case 0x02: // stop push
       pushFlag = 0;
+      sendAck(0x100);
       break;
 
+    case 0x03: // pull (polling mode)
+      sendData(0x180);
+      break;
     } // switch
-  }   // while
+
+    cleanData();
+  } // while
 } // main
 
 // -- Funcion de envio de mensajes de posicion del Cui:
-void send() {
+void sendData(BYTE op) {
   // - Manual: [1. The host issues the command, 0x10. The data read in at this
   // time will be 0xa5 or 0x00 since this is the first SPI transfer]
   LATCbits.LATC2 = 0;
-  WriteSPI(0b00010000); // Solicitación de posición (comando 0x10)
-  Delay10TCYx(5); // Used 4 to fix Known error message (37 b6 ff c4) or (3c 13
-                  // fe c4). Old delay: 3 ( 3 = 6us)
+  WriteSPI(0b00010000); // Solicitacion de posicion (comando 0x10)
+  Delay10TCYx(3); // Used 4 to fix Known error message (37 b6 ff c4) or (3c 13
+                  // fe c4). Delay10TCYx(3)= 6us
   y = SSPBUF; // Recoge los datos del SSPBUF que provienen del encoder (MISO)
   LATCbits.LATC2 = 1;
 
@@ -218,8 +213,8 @@ void send() {
   // command: 0x00]
   LATCbits.LATC2 = 0;
   WriteSPI(0b00000000); // Comando 0x00 -> nop_a5
-  Delay10TCYx(5); // Used 4 to fix Known error message (37 b6 ff c4) or (3c 13
-                  // fe c4). Old delay: 3 ( 3 = 6us)
+  Delay10TCYx(3); // Used 4 to fix Known error message (37 b6 ff c4) or (3c 13
+                  // fe c4). Delay10TCYx(3) = 6us
   y = SSPBUF; // Recoge los datos del SSPBUF que provienen del encoder (MISO)
   LATCbits.LATC2 = 1;
 
@@ -231,8 +226,8 @@ void send() {
     Delay10TCYx(5);
     LATCbits.LATC2 = 0;
     WriteSPI(0b00000000); // Comando 0x00 -> nop_a5)
-    Delay10TCYx(5); // Used 4 to fix Known error message (37 b6 ff c4) or (3c 13
-                    // fe c4). Old delay: 3 ( 3 = 6us)
+    Delay10TCYx(3); // Used 4 to fix Known error message (37 b6 ff c4) or (3c 13
+                    // fe c4). Delay10TCYx(3) = 6us
     y = SSPBUF; // Recoge los datos del SSPBUF que provienen del encoder (MISO)
     LATCbits.LATC2 = 1;
   }
@@ -242,22 +237,22 @@ void send() {
   // data read is the high byte of the position.]
   LATCbits.LATC2 = 0;
   WriteSPI(0b00000000); // Comando 0x00 -> nop_a5
-  Delay10TCYx(5); // Used 4 to fix Known error message (37 b6 ff c4) or (3c 13
-                  // fe c4). Old delay: 3 ( 3 = 6us)
+  Delay10TCYx(3); // Used 4 to fix Known error message (37 b6 ff c4) or (3c 13
+                  // fe c4). Delay10TCYx(3) = 6us
   y = SSPBUF; // Recoge los datos del SSPBUF que provienen del encoder (MISO)
   message[0] =
-      y; // Se almacena en el primer elemento del array que se enviará por CAN
+      y; // Se almacena en el primer elemento del array que se enviara por CAN
   LATCbits.LATC2 = 1;
 
   // - Manual: [5. The host waits a minimum of 5 µs then sends “nop_a5”, the
   // data read is the low byte of the position.]
   LATCbits.LATC2 = 0;
   WriteSPI(0b00000000); // Espera para captar la parte baja del mensaje
-  Delay10TCYx(5); // Used 4 to fix Known error message (37 b6 ff c4) or (3c 13
-                  // fe c4). Old delay: 3 ( 3 = 6us)
+  Delay10TCYx(3); // Used 4 to fix Known error message (37 b6 ff c4) or (3c 13
+                  // fe c4). Delay10TCYx(3) = 6us
   y = SSPBUF; // Recoge los datos del SSPBUF que provienen del encoder (MISO)
   message[1] =
-      y; // Se almacena en el segundo elemento del array que se enviará por CAN
+      y; // Se almacena en el segundo elemento del array que se enviara por CAN
 
   // - Manual: [6. The host waits a minimum of 5 µs before sending another SPI
   // command.]
@@ -268,7 +263,14 @@ void send() {
 
   x = 0;
   while (!x) {
-    x = ECANSendMessage(canId, &degrees, sizeof(degrees), txFlags);
+    x = ECANSendMessage(op + canId, &degrees, sizeof(degrees), txFlags);
+  }
+}
+
+void sendAck(BYTE op) {
+  x = 0;
+  while (!x) {
+    x = ECANSendMessage(op + canId, NULL, 0, txFlags);
   }
 }
 
