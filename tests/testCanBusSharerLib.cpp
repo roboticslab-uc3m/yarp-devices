81acdc783aedca76259bb6681fa05e660079d956
Bartek Łukawski <bwmn.peter@gmail.com>
2019-10-05 20:35:16 +0200

    Prepare CAN library unit test files and config

---
+++ tests/testCanBusSharerLib.cpp
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+
+namespace roboticslab
+{
+
+/**
+ * @ingroup yarp_devices_tests
+ * @brief ...
+ */
+class CanBusSharerTest : public testing::Test
+{
+public:
+
+    virtual void SetUp()
+    {
+
+    }
+
+    virtual void TearDown()
+    {
+
+    }
+
+protected:
+
+};
+
+TEST_F(CanBusSharerTest, CanBusSharerTestDummy)
+{
+
+}
+
+} // namespace roboticslab
