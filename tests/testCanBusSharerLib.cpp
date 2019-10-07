556afff4f112abcb10b723ecf296ec077c152c86
Bartek Łukawski <bwmn.peter@gmail.com>
2019-10-07 18:56:26 +0200

    Test StateObserver and TypedStateObserver<T>

--- tests/testCanBusSharerLib.cpp
+++ tests/testCanBusSharerLib.cpp
@@ -2,7 +2,12 @@
 
 #include <cstdint>
 
+#include <chrono>
+#include <functional>
+#include <thread>
+
 #include "CanUtils.hpp"
+#include "StateObserver.hpp"
 
 namespace roboticslab
 {
@@ -29,10 +34,30 @@ protected:
 
 };
 
+class observer_timer
+{
+public:
+    observer_timer(int _milliseconds, const std::function<bool()> & _fn)
+        : milliseconds(_milliseconds), fn(_fn)
+    { }
+
+    void operator()()
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
+        ASSERT_TRUE(fn());
+    }
+
+private:
+    int milliseconds;
+    const std::function<bool()> & fn;
+};
+
 TEST_F(CanBusSharerTest, CanUtils)
 {
     // http://www.hugi.scene.org/online/coding/hugi%2015%20-%20cmtadfix.htm
     // https://planetcalc.com/862/
+
+    // test CanUtils::encodeFixedPoint(double, std::int8_t *, std::uint8_t *)
 
     double v1 = 3.14;
     std::int8_t int1;
@@ -41,6 +66,8 @@ TEST_F(CanBusSharerTest, CanUtils)
     ASSERT_EQ(int1, 3);
     ASSERT_EQ(frac1, 36);
 
+    // test CanUtils::encodeFixedPoint(double, std::int32_t *, std::uint16_t *)
+
     double v2 = 4444.4444;
     std::int32_t int2;
     std::uint16_t frac2;
@@ -48,15 +75,94 @@ TEST_F(CanBusSharerTest, CanUtils)
     ASSERT_EQ(int2, 4444);
     ASSERT_EQ(frac2, 29124);
 
+    // test CanUtils::decodeFixedPoint(std::int8_t, std::uint8_t)
+
     std::int8_t int3 = 0;
     std::uint8_t frac3 = 204;
     double v3 = CanUtils::decodeFixedPoint(int3, frac3); // 0.11001100
     ASSERT_EQ(v3, 0.796875);
 
+    // test CanUtils::decodeFixedPoint(std::int16_t, std::uint16_t)
+
     std::int16_t int4 = -4444;
     std::uint16_t frac4 = 4444;
     double v4 = CanUtils::decodeFixedPoint(int4, frac4);
     ASSERT_NEAR(v4, -4444.06781, 1e-6);
+}
+
+TEST_F(CanBusSharerTest, StateObserver)
+{
+    const double timeout = 0.1; // [s]
+    const int millis = 10;
+
+    // test StateObserver
+
+    StateObserver emptyStateObserver(timeout);
+    std::thread thread1(observer_timer{millis, [&] { return emptyStateObserver.notify(); }});
+    ASSERT_TRUE(emptyStateObserver.await());
+    thread1.join();
+
+    // test TypedStateObserver<dummy> with dummy = user-defined type
+
+    struct dummy
+    { int val; };
+
+    dummy d;
+    const dummy _v1{4};
+    TypedStateObserver<dummy> dummyStateObserver(timeout);
+    std::thread thread2(observer_timer{millis, [&] { return dummyStateObserver.notify(_v1); }});
+    ASSERT_TRUE(dummyStateObserver.await(d));
+    thread2.join();
+    ASSERT_EQ(d.val, _v1.val);
+
+    // test TypedStateObserver<int>, use notify(int)
+
+    int val1;
+    const int _v2 = 4;
+    TypedStateObserver<int> intStateObserver(timeout);
+    std::thread thread3(observer_timer{millis, [&] { return intStateObserver.notify(_v2); }});
+    ASSERT_TRUE(intStateObserver.await(&val1));
+    thread3.join();
+    ASSERT_EQ(val1, _v2);
+
+    // test TypedStateObserver<int>, use notify(const std::uint8_t *, std::size_t)
+
+    std::int8_t val2;
+    const std::uint8_t _v3[] = {4};
+    TypedStateObserver<std::int8_t> intBuffStateObserver(timeout);
+    std::thread thread4(observer_timer{millis, [&] { return intBuffStateObserver.notify(_v3, 1); }});
+    ASSERT_TRUE(intBuffStateObserver.await(&val2));
+    thread4.join();
+    ASSERT_EQ(val2, _v3[0]);
+
+    // test TypedStateObserver<std::uint8_t[]>
+
+    std::uint8_t buff[1];
+    const std::uint8_t _v4[] = {4};
+    TypedStateObserver<std::uint8_t[]> bufferStateObserver(timeout);
+    std::thread thread5(observer_timer{millis, [&] { return bufferStateObserver.notify(_v4, 1); }});
+    ASSERT_TRUE(bufferStateObserver.await(buff));
+    thread5.join();
+    ASSERT_EQ(buff[0], _v4[0]);
+
+    // test StateObserver, never call notify()
+
+    StateObserver orphanStateObserver(timeout);
+    ASSERT_FALSE(orphanStateObserver.await());
+
+    // test StateObserver on existing instance, await() but don't notify()
+
+    ASSERT_FALSE(emptyStateObserver.await());
+
+    // test StateObserver on existing instance, await() and notify()
+
+    std::thread thread6(observer_timer{millis, [&] { return emptyStateObserver.notify(); }});
+    ASSERT_TRUE(emptyStateObserver.await());
+    thread6.join();
+
+    // test StateObserver on existing instance, notify() but don't await()
+
+    ASSERT_FALSE(emptyStateObserver.notify());
 }
 
 } // namespace roboticslab
