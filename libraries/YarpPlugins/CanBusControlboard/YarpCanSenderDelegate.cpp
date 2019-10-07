fb0e0a875e54a7cf568bc6e40f0cfc95435665ff
Bartek Łukawski <bwmn.peter@gmail.com>
2019-10-07 19:44:34 +0200

    Move CanSenderDelegate's impl to YARP class, make pure virtual

---
+++ libraries/YarpPlugins/CanBusControlboard/YarpCanSenderDelegate.cpp
@@ -0,0 +1,27 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "YarpCanSenderDelegate.hpp"
+
+#include <cstring>
+
+using namespace roboticslab;
+
+bool YarpCanSenderDelegate::prepareMessage(const can_message & msg)
+{
+    std::lock_guard<std::mutex> lock(bufferMutex);
+
+    if (preparedMessages < maxSize)
+    {
+        yarp::dev::CanMessage & message = buffer[preparedMessages++]; // access, then increment
+
+        message.setId(msg.id);
+        message.setLen(msg.len);
+        std::memcpy(message.getData(), msg.data, msg.len * sizeof(msg.data));
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
