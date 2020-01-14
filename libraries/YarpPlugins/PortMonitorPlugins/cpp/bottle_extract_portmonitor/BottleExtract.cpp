bff8c029523701537bb000cdf5cec05d8620e93d
jgvictores <jcgvicto@gmail.com>
2020-01-14 12:57:24 +0100

    parse and extract index

--- libraries/YarpPlugins/PortMonitorPlugins/cpp/bottle_extract_portmonitor/BottleExtract.cpp
+++ libraries/YarpPlugins/PortMonitorPlugins/cpp/bottle_extract_portmonitor/BottleExtract.cpp
@@ -2,6 +2,9 @@
 
 #include "BottleExtract.hpp"
 
+#include <algorithm>
+#include <sstream>
+
 #include <yarp/os/Bottle.h>
 #include <yarp/os/Things.h>
 #include <yarp/os/Log.h>
@@ -14,6 +17,26 @@ bool BottleExtract::create(const yarp::os::Property& options)
    yDebug("created!\n");
    yDebug("I am attached to the %s\n",
           (options.find("sender_side").asBool()) ? "sender side" : "receiver side");
+   std::stringstream parsable(options.find("carrier").asString()); // yDebug("%s\n", parsable.str().c_str());
+   std::string betweenPluses;
+   std::string textForProperty;
+   while(std::getline(parsable, betweenPluses, '+'))
+   {
+       textForProperty.append("(");
+       std::replace(betweenPluses.begin(), betweenPluses.end(), '.', ' ');
+       textForProperty.append(betweenPluses); // yDebug("** %s\n", betweenPluses.c_str());
+       textForProperty.append(") ");
+   }
+   yarp::os::Property parsed;
+   parsed.fromString(textForProperty); // yDebug("textForProperty: %s\n", textForProperty.c_str());
+
+   if(!parsed.check("index"))
+   {
+       yError("Missing 'index', bye!\n");
+       return false;
+   }
+   int index = parsed.find("index").asInt32();
+   yDebug("Using index: %d", index);
    return true;
 }
 
